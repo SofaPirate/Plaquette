7f791eaacd3b6a163660772a71758f6b46fdb9b4
Tats <info@sofianaudry.com>
2017-02-24 17:45:45 -0500

    Example with servo-motor.

---
+++ examples/Pq_Servo/Pq_Servo.ino
@@ -0,0 +1,18 @@
+#include <Plaquette.h>
+#include <PqServo.h>
+
+// Controls the rate of change of the servo.
+const float RATE = 0.01;
+
+// The servo-motor output.
+ServoOut out;
+
+void begin() {
+  // Centers the servo.
+  out.center();
+}
+
+void run() {
+  // Updates the value and send it back as output.
+  (out + randomUniform(-RATE, RATE)) >> out;
+}
