94dbc62d3032e09b6d7229a123964b4f3a7f08cd
Sofian Audry <info@sofianaudry.com>
2022-08-16 14:07:36 -0400

    Updated config file for documentation with most recent version of Sphinx.

--- extras/docs/conf.py
+++ extras/docs/conf.py
@@ -3,6 +3,8 @@ import os
 import shlex
 import subprocess
 
+import sphinx_rtd_theme
+
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
@@ -10,8 +12,8 @@ if read_the_docs_build:
 
 html_sidebars = { '**': ['fulltoc.html', 'relations.html', 'searchbox.html'], }
 
-html_logo = "images/Plaquette-LogoText.png"
-html_favicon = "images/plaquette-favicon.ico"
+html_logo = "Plaquette-LogoText.png"
+html_favicon = "plaquette-favicon.ico"
 html_theme = "sphinx_rtd_theme"
 html_style = None
 html_theme_options = { 'collapse_navigation' : True }
@@ -19,22 +21,20 @@ using_rtd_theme = True
 
 html_show_sourcelink = False
 
-html_context = {
-    'css_files': ['_static/css/custom.css'],
-}
+html_css_files = ['css/custom.css']
 
-extensions = ['breathe', 'sphinx.ext.intersphinx' ]
+extensions = ['breathe', 'sphinx.ext.intersphinx', 'sphinx_rtd_theme' ]
 breathe_projects = { 'Plaquette': 'xml' }
 breathe_default_project = "Plaquette"
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'Plaquette'
-copyright = u'2015-2019, Plaquette'
+copyright = u'2015-2022, Plaquette'
 author = u'Sofian Audry & Thomas O. Fredericks'
 version = '0.2.5'
 release = '0.2.5'
-language = None
+language = 'en'
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 todo_include_todos = False
