e028a45a623ec6ab464366ae2d52d874a456dfb3
Sofian Audry <info@sofianaudry.com>
2022-09-03 22:56:27 -0400

    Rewrote normalizer.ino test using randomNormal() function.

--- tests/normalizer/normalizer.ino
+++ tests/normalizer/normalizer.ino
@@ -4,45 +4,103 @@
 
 using namespace pq;
 
+float randomNormal(float mean=0, float stdv=1) {
+  static bool normal_is_valid = false;
+  static float normal_x;
+  static float normal_y;
+  static float normal_rho;
+  if (!normal_is_valid) {
+    normal_x = randomUniform();
+    normal_y = randomUniform();
+    normal_rho = sqrt(-2. * log(1.0 - normal_y));
+    normal_is_valid = true;
+  } else
+    normal_is_valid = false;
+
+  if (normal_is_valid)
+    return normal_rho * cos(2. * PI * normal_x) * stdv + mean;
+  else
+    return normal_rho * sin(2. * PI * normal_x) * stdv + mean;
+}
+
 #define N_UNITS 3
 Node* units[N_UNITS] = {
   new Normalizer(0, 1),
   new Normalizer(0, 1, 0.5f),
   new Normalizer(0, 1, 1.0f)
 };
+// 
+// testing(valuesInRange) {
+//   static unsigned long startTime = millis();
+//   static bool initialized = false;
+//   static int currentRandom = 0;
+//   static float currentSign = 1;
+//
+//   if (!initialized) {
+//     for (int i=0; i<N_UNITS; i++) {
+//       Normalizer* unit =(Normalizer*) units[i];
+//       unit->reset();
+//     }
+//   }
+//
+//   Plaquette.step();
+//
+//   float randomValue = randomNormal();
+//
+//   for (int i=0; i<N_UNITS; i++) {
+//     Normalizer* unit =(Normalizer*) units[i];
+//     float value = unit->put(randomValue);
+//
+//     if (millis() - startTime > 1000) {
+// //      Serial.printf("val=%f ==> %f\n", randomValue, value );
+//       assertMoreOrEqual(value, -3.1f);
+//       assertLessOrEqual(value,  3.1f);
+//
+//       assertEqual(unit->isLowOutlier(-100, 1.0f), true);
+//       assertEqual(unit->isHighOutlier(100, 1.0f), true);
+//       assertEqual(unit->isOutlier(-100, 1.0f), true);
+//       assertEqual(unit->isOutlier(-100, 1.0f), true);
+//     }
+//
+//     // if (millis() - startTime > 4000) {
+//     //   assertNear(unit->normalize(100),   1.0f, 0.5f);
+//     //   assertNear(unit->normalize(-100), -1.0f, 0.5f);
+//     // }
+//   }
+//   if (millis() - startTime > 5000) pass();
+// }
 
-testing(valuesInRange) {
+testing(matchRandomNormal) {
   static unsigned long startTime = millis();
+  static bool initialized = false;
   static int currentRandom = 0;
   static float currentSign = 1;
 
+  if (!initialized) {
+    for (int i=0; i<N_UNITS; i++) {
+      Normalizer* unit =(Normalizer*) units[i];
+      unit->reset();
+    }
+    initialized = true;
+  }
+
   Plaquette.step();
 
-  float randomValue = currentRandom * currentSign;
-  currentRandom = (currentRandom + 1) % 101;
-  currentSign = -currentSign;
+  float randomValue = randomNormal();
 
   for (int i=0; i<N_UNITS; i++) {
     Normalizer* unit =(Normalizer*) units[i];
-    float value = unit->put(randomValue);
-
-    if (millis() - startTime > 1000) {
-//      Serial.printf("val=%f ==> %f\n", randomValue, value );
-      assertMoreOrEqual(value, -3.1f);
-      assertLessOrEqual(value,  3.1f);
-
-      assertEqual(unit->isLowOutlier(-100, 1.0f), true);
-      assertEqual(unit->isHighOutlier(100, 1.0f), true);
-      assertEqual(unit->isOutlier(-100, 1.0f), true);
-      assertEqual(unit->isOutlier(-100, 1.0f), true);
-    }
-
-    // if (millis() - startTime > 4000) {
-    //   assertNear(unit->normalize(100),   1.0f, 0.5f);
-    //   assertNear(unit->normalize(-100), -1.0f, 0.5f);
-    // }
+    unit->put(randomValue);
+    unit->put(-randomValue);
   }
-  if (millis() - startTime > 5000) pass();
+  if (millis() - startTime > 5000) {
+    for (int i=0; i<N_UNITS; i++) {
+      Normalizer* unit =(Normalizer*) units[i];
+      assertNear(unit->mean(), 0.0f, 0.2f);
+      assertNear(unit->stddev(), 1.0f, 0.2f);
+    }
+    pass();
+  }
 }
 
 void setup() {
