0b5724e3d456eb7fde33016bba518781a748e3e8
Sofian Audry <info@sofianaudry.com>
2025-01-07 23:13:31 -0500

    Updated conf.py for version 0.6.0.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ new_supported_image_types = [
 additional_default_supported_images = list(set(StandaloneHTMLBuilder.supported_image_types) - set(new_supported_image_types))
 StandaloneHTMLBuilder.supported_image_types = new_supported_image_types + additional_default_supported_images
 
-html_sidebars = { '**': ['fulltoc.html', 'relations.html', 'searchbox.html'], }
+html_sidebars = { '**': ['fulltoc.html', 'relations.html', 'searchbox.html'] }
 
 html_logo = "Plaquette-LogoText.png"
 html_favicon = "plaquette-favicon.ico"
@@ -35,17 +35,27 @@ html_show_sourcelink = False
 
 html_css_files = ['css/custom.css']
 
-extensions = ['breathe', 'sphinx.ext.intersphinx', 'sphinx_rtd_theme' ]
+extensions = [
+    'sphinx.ext.autodoc',
+#    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'breathe',
+ #   'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
+]
+#autosummary_generate = True  # Automatically generate summary files
+#autosummary_generate_debug = True
 breathe_projects = { 'Plaquette': 'xml' }
 breathe_default_project = "Plaquette"
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'Plaquette'
-copyright = u'2015-2023, Plaquette'
+copyright = u'2015-2025, Plaquette'
 author = u'Sofian Audry & Thomas O. Fredericks'
-version = '0.5.0'
-release = '0.5.0'
+version = '0.6.0'
+release = '0.6.0'
 language = 'en'
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
