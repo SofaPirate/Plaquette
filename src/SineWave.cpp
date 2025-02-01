19d1c964f176cb79800c355562f6e8f12571b56d
Sofian Audry <info@sofianaudry.com>
2025-02-01 17:37:50 -0500

    Adjusted behavior of sine wave to behave more like triangle waves.

--- src/SineWave.cpp
+++ src/SineWave.cpp
@@ -36,25 +36,31 @@ float SineWave::_get(phase_time_t t) {
   int16_t sineWave16;
   // Special case: width == 0.5 (default and most common). More efficient.
   if (_width == HALF_PHASE_TIME_MAX) {
-    sineWave16 = sin16((uint16_t)(t >> 16));
+    sineWave16 = cos16((uint16_t)(t >> 16));
   }
+
   // General case.
   else {
     // Remapped phase time (16 bits).
     uint16_t remappedPhaseTime16;
-    // Rising part of sine wave.
-    if (t <= _width) {
-      remappedPhaseTime16 = (uint16_t) ((float)t / (_width + FLT_MIN) * 32767.0f);
+    // Special case (falling sine wave / equivalent of inverted sawtooth).
+    if (_width == 0) {
+      remappedPhaseTime16 = (uint16_t) ((float)(t) / PHASE_TIME_MAX * 32767.0f) + 32768;
     }
-    // Falling part of sine wave
+    // Rising part of sine wave.
+    else if (t <= _width) {
+      remappedPhaseTime16 = (uint16_t) ((float)t / _width * 32767.0f);
+    }
+    // Falling part of sine wave.
     else {
       phase_time_t widthMinusOne = _width - 1;
       remappedPhaseTime16 = (uint16_t) ((float)(t - widthMinusOne) / (PHASE_TIME_MAX - widthMinusOne) * 32767.0f) + 32768;
     }
-    sineWave16 = sin16(remappedPhaseTime16);
+    sineWave16 = cos16(remappedPhaseTime16);
   }
   
-  return (uint16_t(32767) + sineWave16) / 65534.0f;
+  // Convert to [0, 1] with wave shape similar to triangle wave.
+  return (uint16_t(32767) - sineWave16) / 65534.0f; // = ( 32767 - cos16(remappedPhaseTime16) ) / 65534
 }
 
 }
