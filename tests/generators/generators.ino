e6d85b317d46f26ed92d491714c126568bb3b22b
Sofian Audry <info@sofianaudry.com>
2026-01-05 00:26:09 -0600

    Added unit test for passedSkew() and passedPeriod().

--- tests/generators/generators.ino
+++ tests/generators/generators.ino
@@ -5,7 +5,7 @@
 using namespace pq;
 
 #define N_GENERATORS 6
-AbstractWave* generators[N_GENERATORS] = {
+Wave* generators[N_GENERATORS] = {
   new Wave(0.005),
   new Wave(1),
   new Wave(TRIANGLE, 0.005),
@@ -62,14 +62,84 @@ testing(valuesIn01) {
   }
 }
 
+Engine engine2;
+
+Wave* waves[N_GENERATORS] = {
+  new Wave(TRIANGLE, 1.0, 0, engine2),
+  new Wave(TRIANGLE, 1.0, 0.5, engine2),
+  new Wave(TRIANGLE, 1.0, 1, engine2),
+  new Wave(TRIANGLE, 1.0, 0, engine2),
+  new Wave(TRIANGLE, 1.0, 0.5, engine2),
+  new Wave(TRIANGLE, 1.0, 1, engine2)
+};
+
+int countSkew[N_GENERATORS] = { 0, 0, 0, 0, 0, 0 };
+int countPeriod[N_GENERATORS] =  { 0, 0, 0, 0, 0, 0 };
+
+#define TOLERANCE 0.1f
+testing(waveEvents) {
+  static float startTime = -1;
+  static const float RUNTIME = 5.0f;
+  if (startTime < 0)
+    startTime = engine2.seconds();
+
+  engine2.step();
+
+
+  for (int i=0; i<N_GENERATORS; i++) {
+    Wave* wave = waves[i];
+
+    // Serial.print(i); Serial.print(" "); Serial.println(wave->get());
+
+
+    if (wave->passedSkew()) {
+      float expectedValue;
+      if ((wave->isForward() && wave->skew() == 1) || (!wave->isForward() && wave->skew() == 0))
+        expectedValue = 0;
+      else
+        expectedValue = 1;
+      assertNear(wave->get(), expectedValue, TOLERANCE);
+      countSkew[i]++;
+    }
+    if (wave->passedPeriod()) {
+      float expectedValue;
+      if ((wave->isForward() && wave->skew() == 0) || (!wave->isForward() && wave->skew() == 1))
+        expectedValue = 1;
+      else
+        expectedValue = 0;
+      assertNear(wave->get(), expectedValue, TOLERANCE);
+      countPeriod[i]++;
+    }
+
+    if (wave->skew() == 0 || wave->skew() == 1)
+      assertEqual((bool)(wave->passedPeriod() ^ wave->passedSkew()), false);
+    else
+      assertEqual(wave->passedPeriod() && wave->passedSkew(), false);
+  }
+
+  if (engine2.seconds() - startTime > RUNTIME) {
+    for (int i=0; i<N_GENERATORS; i++) {
+      Wave* wave = waves[i];
+      assertNear((float)countSkew[i], RUNTIME / wave->period(), 1 / (wave->period() * wave->period()));
+    }
+    pass();
+  }
+}
+
 test(nUnits) {
   assertEqual((int)Plaquette.nUnits(), N_GENERATORS*2);
+  assertEqual((int)engine2.nUnits(), N_GENERATORS);
 }
 
 void setup() {
   Plaquette.begin();
   for (int i=0; i<N_GENERATORS; i++) {
     detectors[i]->reloadThreshold(PEAK_DETECTOR_THRESHOLD-0.1f);
+  }
+
+  engine2.begin();
+  for (int i=3; i<N_GENERATORS; i++) {
+    waves[i]->reverse();
   }
 }
 
