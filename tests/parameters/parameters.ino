b89705c1e3e0a62fb12f585592269c0d7d939fdb
Sofian Audry <info@sofianaudry.com>
2026-01-10 12:51:03 -0500

    Fixed renaming of parameter functions in unit test.

--- tests/parameters/parameters.ino
+++ tests/parameters/parameters.ino
@@ -10,36 +10,36 @@ Ramp ramp(10);
 AbstractTimer* timer = &ramp;
 
 test(parameters) {
-  10 >> squareWave.period();
+  10 >> squareWave.Period();
   assertEqual(10.0f, squareWave.period());
 }
 
 test(virtualParameters) {
-  2 >> ramp.duration();
+  2 >> ramp.Duration();
   assertEqual(2.0f, ramp.duration());
   assertEqual(ramp.mode(), RAMP_DURATION);
 
-  2 >> ramp.speed();
+  2 >> ramp.Speed();
   assertEqual(2.0f, ramp.speed());
   assertEqual(ramp.mode(), RAMP_SPEED);
 
-  2 >> timer->duration();
+  2 >> timer->Duration();
   assertEqual(2.0f, ramp.duration());
   assertEqual(2.0f, timer->duration());
   assertEqual(ramp.mode(), RAMP_DURATION);
 }
 
 test(reference) {
-  Wave::Parameter period = squareWave.period();
-  Wave::Parameter frequency = squareWave.frequency();
+  Wave::Parameter period = squareWave.Period();
+  Wave::Parameter frequency = squareWave.Frequency();
   for (float p = 1; p < 10; p ++) {
     p >> period;
     assertNear(period, frequencyToPeriod(frequency), 0.00001);
   }
 }
 
-auto period = squareWave.period();
-auto frequency = squareWave.frequency();
+auto period = squareWave.Period();
+auto frequency = squareWave.Frequency();
 testing(global) {
   (period + 1) >> period;
   assertNear(period, frequencyToPeriod(frequency), 0.00001);
